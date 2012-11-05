a0bb346e78bad3356ba5fe5abcea7eedb5459bbe
David Turner <david@davidturner.name>
2012-11-05 18:35:47 +0000

    Initial Commit
    Uploading both the jQuery and minified jQuery files.

---
+++ jquery.select.js
@@ -0,0 +1,30 @@
+(function($){
+  $.fn.extend({
+    customStyle : function(options) {
+      var defaults = {
+          selClass: 'custom-select',
+          labelClass: 'custom-select-label'
+      };
+      options =  $.extend(defaults, options);
+      if(!$.browser.msie){
+        return this.each(function() {
+            var select = $(this),
+                val = select.find(":selected"),
+                name = select.attr('id'),
+                o = options,
+                label, time;
+          if(typeof name === 'undefined'){
+            time =  new Date();
+            name = 'select-id-' + time.getTime();
+            select.attr('id', name);
+          }
+          select.addClass(o.selClass).after('<label class="'+o.labelClass+'" for="' + name + '">'+val.text()+'</label>');
+          label = select.next();
+          select.change(function(){
+            label.text(select.val());
+          });
+        });
+      }
+    }
+  });
+})(jQuery);
